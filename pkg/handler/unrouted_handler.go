e15b74946969f90356904c7d8f434c7fec789ce1
wufuqiang <285127760@qq.com>
2023-03-24 15:51:40 +0800

    Update unrouted_handler.go
    增加文件信息返回

--- pkg/handler/unrouted_handler.go
+++ pkg/handler/unrouted_handler.go
@@ -291,6 +291,7 @@ func (handler *UnroutedHandler) PostFile(w http.ResponseWriter, r *http.Request)
 	fileId := r.Header.Get("unique_id")
 	with_ok := r.Header.Get("with_ok")
 	if with_ok != "" {
+		w.Header().Set("FileInfo", with_ok)
 		handler.sendResp(w, r, http.StatusConflict)
 		return
 	}
